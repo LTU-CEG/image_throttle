b3de129a267bb0e86001e42c0f53516ce87316da
Emil Fresk <emil.fresk@gmail.com>
2017-12-06 22:47:35 +0100

    Added license

--- src/image_throttle_node.cpp
+++ src/image_throttle_node.cpp
@@ -1,3 +1,8 @@
+//          Copyright Emil Fresk 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE.md or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
